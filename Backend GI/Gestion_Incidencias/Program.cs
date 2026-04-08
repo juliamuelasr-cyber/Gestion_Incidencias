4fedcad1e31379443b23f78dc2388e94442fdc3c
Guada <guadalupe.fernandez@des.kyocera.com>
2026-04-08 15:23:55 +0200

    repositorio conectado a db

--- Backend GI/Gestion_Incidencias/Program.cs
+++ Backend GI/Gestion_Incidencias/Program.cs
@@ -7,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddScoped<Kyocera.Microservice.WebAPI.Interface.IInterface, Kyocera.Microservice.DbContext.Repositorios.IncidenciaRepositorio>();
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ Backend GI/Kyocera.Microservice.DbContext/Repositorios/IncidenciaRepositorio.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Kyocera.Microservice.Models.Models;
+using Kyocera.Microservice.DbContext.BoundedContext;
+using Kyocera.Microservice.WebAPI.Interface; // Referencia a la interfaz
+
+namespace Kyocera.Microservice.DbContext.Repositorios
+{
+    public class IncidenciaRepositorio : IInterface
+    {
+        private readonly AppDbContext _context;
+
+        public IncidenciaRepositorio(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Incidencia>> GetAllAsync()
+        {
+            return await _context.Incidencias.ToListAsync();
+        }
+
+        public async Task<Incidencia> GetByIdAsync(int id)
+        {
+            return await _context.Incidencias.FindAsync(id);
+        }
+
+        public async Task CreateAsync(Incidencia incidencia)
+        {
+            await _context.Incidencias.AddAsync(incidencia);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Incidencia incidencia)
+        {
+            _context.Incidencias.Update(incidencia);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var incidencia = await _context.Incidencias.FindAsync(id);
+            if (incidencia != null)
+            {
+                _context.Incidencias.Remove(incidencia);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
