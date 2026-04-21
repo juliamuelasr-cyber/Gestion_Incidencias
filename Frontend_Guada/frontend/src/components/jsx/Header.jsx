c9ca2ba3c5ea75b2c1bc6d734eba92cd5059e096
Guada <guadalupe.fernandez@des.kyocera.com>
2026-04-21 13:10:27 +0200

    Arreglado Front

--- Frontend_Guada/frontend/src/components/jsx/Header.jsx
+++ Frontend_Guada/frontend/src/components/jsx/Header.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import { LogOut } from 'lucide-react';
-import logoImg from '../assets/Kyocera_logo.svg.png'; 
+import logoImg from '../../assets/Kyocera_logo.svg.png'; 
 import '../css/Header.css';
 
 export default function Header({ onLogout }) {
